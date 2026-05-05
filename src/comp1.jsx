0db3f7dff13e938aef9c81b58362a3b59891bcf4
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-05-05 23:09:02 +0530

    Delete src directory

--- src/comp1.jsx
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-class Comp1 extends Component {
-    render() {
-        return (
-            <h1>This is Component1</h1>
-        )
-    }
-}
-export default Comp1;
