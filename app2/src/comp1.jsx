048a81fa889bef1ebe08e5f60409f274e476bb34
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2026-05-05 23:10:52 +0530

    Add files via upload

---
+++ app2/src/comp1.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+class Comp1 extends Component {
+    render() {
+        return (
+            <h1>This is Component1</h1>
+        )
+    }
+}
+export default Comp1;
